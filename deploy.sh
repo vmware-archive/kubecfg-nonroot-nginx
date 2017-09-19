193f56e4e8b08e59de5c8a493fb1b470fe61db3c
Jorge Bianquetti <jbianquetti@bitnami.com>
2017-09-19 07:30:49 +0200

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,11 @@ logmsg () {
 } 
 
 if [ -f nginx.jsonnet ] ; then
-	logmsg 'Showing compiled items...'
-	kubecfg show nginx.jsonnet 
 	logmsg 'Showing changes...'
 	# The `echo` command  force to do not fail if object does not exist yet
 	# or local state it's different than remote server
 	kubecfg diff --diff-strategy subset nginx.jsonnet ; echo 
+	# Updating will occur as a deployment.
+	logmsg  'Updating items on the cluster...'
+	kubecfg update nginx.jsonnet 
 fi
